bbdeedfe3f365a709145e3f10f660e08d2cf9380
Tiril Grimsrud <tiril@Tirils-MacBook-Pro.local>
2018-01-05 13:50:33 +0100

    loading screen with js working

---
+++ script.js
@@ -0,0 +1,9 @@
+//loading circle
+
+let overlay = document.getElementById("overlay");
+
+window.addEventListener('load', function () {
+    console.log('loading');
+    overlay.style.display = 'none';
+
+})
