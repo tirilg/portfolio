c036188d0fa5987779c5f7e96c182a3300248b3b
Tiril Grimsrud <tiril@Tirils-MacBook-Pro.local>
2018-01-05 15:26:12 +0100

    working

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-//loading circle
-
 let overlay = document.getElementById("overlay");
-
+//loading screen
 window.addEventListener('load', function () {
     console.log('loading');
     overlay.style.display = 'none';
