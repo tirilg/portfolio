543e70ac449da94811ce7fb5509c9244920e98ac
Tiril Grimsrud <tiril@Tirils-MacBook-Pro.local>
2018-01-06 20:46:34 +0100

    horizontal scroll deactivated working

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 let overlay = document.getElementById("overlay");
+let scrollEventHandler = function ()
+
+{
+    window.scroll(0, window.pageYOffset)
+}
+
+window.addEventListener("scroll", scrollEventHandler, false);
 //loading screen
 window.addEventListener('load', function () {
     console.log('loading');
